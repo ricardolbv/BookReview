16e6cffbc5d2ec5503a8dd326ad5d362b59f2ec7
Ricardo Vieira <ricardo.biscola.vieira@gmail.com>
2022-03-15 21:06:22 -0300

    First commit

---
+++ BookReview.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BookReview.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ BookReview.Persistence/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BookReview.Persistence
+{
+    public class Class1
+    {
+    }
+}
