ceb7d5db30792595e3e42dbf1b65e8f839b20c86
Ricardo Vieira <ricardo.biscola.vieira@gmail.com>
2022-03-21 19:59:59 -0300

    Unit test for: Create a review

---
+++ BookReview.Application.UnitTests/Reviews/Commands/CreateReviewCommandHandlerTests.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using BookReview.Application.Contracts.Persistence;
+using BookReview.Application.Features.Reviews.Commands.CreateReview;
+using BookReview.Application.Profiles;
+using BookReview.Application.UnitTests.Mocks;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BookReview.Application.UnitTests.Reviews.Commands
+{
+    public class CreateReviewCommandHandlerTests
+    {
+        private readonly Mock<IReviewRepository> _repo;
+        private readonly IMapper _mapper;
+
+        public CreateReviewCommandHandlerTests()
+        {
+            _repo = RepositoryMocks.GetMockReviewRepository();
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MapperProfile>();
+            });
+
+            _mapper = config.CreateMapper();
+        }
+
+        [Fact]
+        public async Task Create_Valid_Review()
+        {
+            var handler = new CreateReviewCommandHandler(_mapper, _repo.Object);
+
+            var resp = await handler.Handle(new CreateReviewCommand()
+            {
+                Text = "Testing ekofjowifoijwrijgio  iorgjio eroi g" +
+                "test sett "
+            }, CancellationToken.None);
+
+            var reviews = await _repo.Object.ListAllAsync();
+
+            //Asserts
+            reviews.Count.ShouldBe(4);
+            resp.State.ShouldNotBeSameAs("Created");
+        }
+    }
+}
