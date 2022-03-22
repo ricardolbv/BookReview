9719101d3fab7236b39cc30ad00954f1141ed55e
Ricardo Vieira <ricardo.biscola.vieira@gmail.com>
2022-03-21 21:39:21 -0300

    [Unit test] - Unit test for: Delete a review - Need to implement in mock repo

---
+++ BookReview.Application.UnitTests/Reviews/Commands/DeleteReviewByIdCommandTests.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using BookReview.Application.Contracts.Persistence;
+using BookReview.Application.Features.Reviews.Commands.DeleteReviewById;
+using BookReview.Application.Profiles;
+using BookReview.Application.UnitTests.Mocks;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BookReview.Application.UnitTests.Reviews.Commands
+{
+    public class DeleteReviewByIdCommandTests
+    {
+        private readonly Mock<IReviewRepository> _repo;
+        private readonly IMapper _mapper;
+
+        public DeleteReviewByIdCommandTests()
+        {
+            _repo = RepositoryMocks.GetMockReviewRepository();
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MapperProfile>();
+            });
+
+            _mapper =  config.CreateMapper();
+        }
+
+        [Fact]
+        public async Task Can_Delete_Review_By_Id()
+        {
+            var handler = new DeleteReviewByIdCommandHandler(_mapper, _repo.Object);
+            var resp = await handler.Handle(new DeleteReviewByIdCommand { Id = 1}, CancellationToken.None);
+
+            //Asserts
+            var reviews = await _repo.Object.ListAllAsync();
+
+            reviews.Count.ShouldBe(2);
+            resp.ShouldBe(1);
+        }
+    }
+}
