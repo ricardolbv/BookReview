a2f6161c0e56ca4f0b478e10ad2f0f769376942f
Ricardo Vieira <ricardo.biscola.vieira@gmail.com>
2022-04-13 21:04:41 -0300

    Adding logs to a local file with Serilog

--- BookReview.Api/Program.cs
+++ BookReview.Api/Program.cs
@@ -2,8 +2,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Formatting.Json;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,9 +15,32 @@ namespace BookReview.Api
 {
     public class Program
     {
+        public static IConfiguration _Configuration { get; } = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true)
+           .AddEnvironmentVariables()
+           .Build();
+
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(_Configuration)
+                .WriteTo.File(new JsonFormatter(), @"c:\temp\logs\bookreview-club.json", shared: true)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
